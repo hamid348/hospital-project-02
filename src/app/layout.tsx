48c2c9a7d4c5c66f777934fd9939cae2ef419d66
hamidraza <hamid786raz@gmail.com>
2025-02-06 12:40:26 +0500

    whatsapp float button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import WhatsAppFloat from "@/components/WhatsAppFloat";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={` ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <WhatsAppFloat/>
       </body>
     </html>
   );
